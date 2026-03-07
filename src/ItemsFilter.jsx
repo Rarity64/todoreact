f7f1afd5d7807f0e05948124646aa6d103050aca
EduardSuyargulov <edsuyargulov@gmail.com>
2026-03-07 16:08:58 +0500

    Добавление состояний

--- src/ItemsFilter.jsx
+++ src/ItemsFilter.jsx
@@ -1,16 +1,20 @@
-const ItemsFilter = () => {
-    return(
-        <div className="btn-group" role="group">
-            <input type="radio" className="btn-check" name="btnradio" id="btnradio1"/>
-            <label className="btn btn-outline-info" htmlFor="btnradio1">Всё</label>
+import React from 'react';
 
-            <input type="radio" className="btn-check" name="btnradio" id="btnradio2"/>
-            <label className="btn btn-outline-primary" htmlFor="btnradio2">Активное</label>
+class ItemsFilter extends React.Component {
+    render() {
+        return(
+            <div className="btn-group" role="group">
+                <input type="radio" className="btn-check" name="btnradio" id="btnradio1"/>
+                <label className="btn btn-outline-info" htmlFor="btnradio1">Всё</label>
 
-            <input type="radio" className="btn-check" name="btnradio" id="btnradio3"/>
-            <label className="btn btn-outline-success" htmlFor="btnradio3">Сделанное</label>
-        </div>
-    )
+                <input type="radio" className="btn-check" name="btnradio" id="btnradio2"/>
+                <label className="btn btn-outline-primary" htmlFor="btnradio2">Активное</label>
+
+                <input type="radio" className="btn-check" name="btnradio" id="btnradio3"/>
+                <label className="btn btn-outline-success" htmlFor="btnradio3">Сделанное</label>
+            </div>
+        )
+    }
 }
 
 export default ItemsFilter;
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -6,12 +6,6 @@ const TodoList = ({todos}) => {
         return(
             <li className="list-group-item" key={item.id}>
                 <TodoListItem {...item}/>
-                <button type="button" className="btn btn-outline-success my-button">
-                    <i className="fa-solid fa-check"></i>
-                </button>
-                <button type="button" className="btn btn-outline-danger my-button">
-                    <i className="fa-solid fa-trash"></i>
-                </button>
             </li>
         )
     });
